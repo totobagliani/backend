e218f987b854cc5806761584bd0af22ad3e45883
Tomas Bagliani <tomybagliani@gmail.com>
2023-01-01 19:26:51 +0100

    add index and data of products

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
+const products = require('./products-data');
+
+const app = express();
+
+/** Middlewares */
+// Security
+app.use(cors());
+// Request information from server
+app.use(morgan('dev'));
+// read and Parse body request (from POST and PUT request)
+app.use(express.json());
+// public directory, page in server
+app.use(express.static('public'));
+
+/* --- ROUTES ---- */
+
+app.get('/products', (req, res) => {
+  console.log(products);
+  res.json(products);
+});
+
+/** Server Up */
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server up in port: ${process.env.PORT}`);
+});
---
+++ products-data.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: '595857919392',
+    name: 'Columna Inca',
+    description:
+      'Complemento ideal para cuarto de baño por su capacidad de almacenaje',
+    image: 'https://media.bahag.cloud/m/1280235/12.jpg',
+    price: 79,
+    section: 'baño',
+  },
+  {
+    id: '595857919392',
+    name: 'Columna Inca',
+    description:
+      'Complemento ideal para cuarto de baño por su capacidad de almacenaje',
+    image: 'https://media.bahag.cloud/m/1280235/12.jpg',
+    price: 79,
+    section: 'baño',
+  },
+  {
+    id: '595857919392',
+    name: 'Columna Inca',
+    description:
+      'Complemento ideal para cuarto de baño por su capacidad de almacenaje',
+    image: 'https://media.bahag.cloud/m/1280235/12.jpg',
+    price: 79,
+    section: 'baño',
+  },
+  {
+    id: '595857919392',
+    name: 'Columna Inca',
+    description:
+      'Complemento ideal para cuarto de baño por su capacidad de almacenaje',
+    image: 'https://media.bahag.cloud/m/1280235/12.jpg',
+    price: 79,
+    section: 'baño',
+  },
+];
+
+module.exports = products;
